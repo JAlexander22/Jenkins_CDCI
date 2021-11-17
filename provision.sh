e0e331620108f8ff869451ee192188b5763c6f26
jalexander22 <jalexander@spartaglobal.com>
2021-11-17 19:20:55 +0000

    testb

--- provision.sh
+++ provision.sh
@@ -11,19 +11,19 @@ sudo apt-get install python-software-properties -y
 #Finds a specific verison of Nodejs and installs it again and with pm2
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-sudo npm install pm2 -g
+npm install pm2 -g
 
 #Creates a environment variable and stores in .bashrc file
 sudo echo 'export DB_HOST="mongodb://192.168.10.150:27017/posts"' >> .bashrc
 source ~/.bashrc
 
 #Removes nginx default file and replaces with own default file from home dir
-sudo rm -rf /etc/nginx/sites-available/default
-sudo ln -s /home/ubuntu/Jenkins_CDCI/default /etc/nginx/sites-available/
+rm -rf /etc/nginx/sites-available/default
+ln -s /home/ubuntu/Jenkins_CDCI/default /etc/nginx/sites-available/
 
 #Restarts Nginx
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+systemctl restart nginx
+systemctl enable nginx
 
 # Node seeds app for database
 #node app/app/seeds/seed.js
